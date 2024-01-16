ab2fccd2ce98029ec04f8978fbb5c241b44e0dce
Dekr0 <dekr0.dk@protonmail.com>
2024-01-15 20:44:26 -0700

    update

--- src/solution/arrays-hashing/unique_emails/unique_emails.go
+++ src/solution/arrays-hashing/unique_emails/unique_emails.go
@@ -11,13 +11,53 @@ func NumUniqueEmails(emails []string) int {
     count := 0
 
     m := make(map[string]Void)
+
+    var builder strings.Builder
+
     for _, e := range emails {
-        _, in := map[pp]
+        split := strings.Split(e, "@")
+        local, domain := strings.ReplaceAll(split[0], ".", ""), split[1]
+        ignore_from := strings.IndexByte(local, '+')
+        if ignore_from > -1 {
+            local = local[:ignore_from]
+        }
+        builder.WriteString(local)
+        builder.WriteByte('@')
+        builder.WriteString(domain)
+        pp := builder.String()
+        builder.Reset()
+        _, in := m[pp]
         if !in {
-            map[pp] = Void{}
+            m[pp] = Void{}
             count++
         }
     }
 
-    return 0
+
+    return count
+}
+
+func Tokenizer(emails []string) int {
+    count := 0
+    var builder strings.Builder
+    m := make(map[string]struct{})
+    for _, email := range emails {
+        domain := false
+        for _, char := range email {
+            switch char {
+                case '+':
+                    continue
+                case '@':
+                    domain = true
+                case '.':
+                    if !domain {
+                        continue
+                    }
+                    fallthrough
+                default:
+                   builder.WriteRune(char) 
+            }
+        }
+    }
+    return count
 }
